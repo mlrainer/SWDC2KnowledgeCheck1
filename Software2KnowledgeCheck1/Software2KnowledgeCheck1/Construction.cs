eef00ec6d405b3b8fbcf5329718d5d53f76e05b3
mlrainer <112909069+mlrainer@users.noreply.github.com>
2023-02-26 20:35:22 -0500

    Knowledge Check 1

---
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/Construction.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Software2KnowledgeCheck1
+{
+    internal class Construction
+    {
+        public List<Building> Buildings { get; } = new List<Building>();
+
+        public void CreateApartment(Apartment apartment)
+        {
+            // Get materials
+            var materialRepo = new MaterialsRepo();
+            var materialsNeeded = materialRepo.GetMaterials();
+
+            var permitRepo = new ZoningAndPermitRepo();
+
+            var buildingWasMade = ConstructBuilding<Apartment>(materialsNeeded, permitRepo.GetPermit(), permitRepo.ZoningApproves());
+
+            if (buildingWasMade)
+            {
+                Buildings.Add(apartment);
+            }
+        }
+
+        public bool ConstructBuilding<T>(List<string> materials, bool permit, bool zoning) where T : Building
+        {
+            if (permit && zoning)
+            {
+                foreach (var material in materials)
+                {
+                    if (material == "concrete")
+                    {
+                        // start laying foundation
+                    }
+                    else if (material == "Steel")
+                    {
+                        // Start building structure
+                    }
+                    // etc etc...
+
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
